8018ff99b6ce307652f258605da5bd83997def4e
Roque Buarque <roque.buarque@share-now.com>
2021-04-18 13:08:56 +0200

    show/hide progress bar

--- app/src/main/java/com/devvailonge/flip/MainActivity.kt
+++ app/src/main/java/com/devvailonge/flip/MainActivity.kt
@@ -2,6 +2,7 @@ package com.devvailonge.flip
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import com.devvailonge.flip.databinding.ActivityMainBinding
 import com.devvailonge.flip.features.categories.presentation.CategoryListEvent
@@ -43,7 +44,7 @@ class MainActivity : AppCompatActivity() {
                 state.message
             }
             is CategoryListState.Loading -> {
-                state.isLoading
+               binding.loading.isVisible = state.isLoading
             }
             CategoryListState.Empty -> {
                 binding.imgTest.setImageResource(R.drawable.ic_category_empty)
