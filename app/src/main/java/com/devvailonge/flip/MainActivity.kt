8236cb04c224d6d3c3bb72c6008d7b66965e5462
Roque Buarque <roque.buarque@share-now.com>
2021-04-18 12:39:47 +0200

    fixing loading when fetch category list

--- app/src/main/java/com/devvailonge/flip/MainActivity.kt
+++ app/src/main/java/com/devvailonge/flip/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.devvailonge.flip
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.devvailonge.flip.databinding.ActivityMainBinding
@@ -36,7 +35,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateState(state: CategoryListState) {
-        Log.d("Roqueeee", state.toString())
         when (state) {
             is CategoryListState.CategoryList -> {
                 state.list
