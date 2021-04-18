218245a307f16caf3e5832ece641d8de2822dc2c
devrxp <devrxp@gmail.com>
2021-04-18 16:35:38 -0300

    Display MOCK Category Image List

--- app/src/main/java/com/devvailonge/flip/features/categories/CategoryListFragment.kt
+++ app/src/main/java/com/devvailonge/flip/features/categories/CategoryListFragment.kt
@@ -1,4 +1,19 @@
 package com.devvailonge.flip.features.categories
 
-class CategoryListFragment {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.devvailonge.flip.R
+import com.devvailonge.flip.features.categories.data.generateCategoryList
+
+class CategoryListFragment: AppCompatActivity() {
+
+    lateinit var rvCategoryImageList:RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_category_image_list)
+        rvCategoryImageList = findViewById(R.id.rvCategoryImageList)
+        rvCategoryImageList.adapter = CategoryImageListAdapter(generateCategoryList())
+    }
 }
