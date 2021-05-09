e20443bfa5f89e775149a25a6e9f115f5bc45c2c
Roque Buarque <roque.buarque@share-now.com>
2021-05-09 08:58:25 +0200

    flash card list ui FlashCardListFragment

--- app/src/main/java/com/devvailonge/flip/features/flashcard/list/presentation/ui/FlashCardListFragment.kt
+++ app/src/main/java/com/devvailonge/flip/features/flashcard/list/presentation/ui/FlashCardListFragment.kt
@@ -1,11 +1,28 @@
 package com.devvailonge.flip.features.flashcard.list.presentation.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.devvailonge.flip.R
+import com.devvailonge.flip.databinding.FragmentFlashcardListBinding
+import com.devvailonge.flip.utils.viewBinding
 
-class FlashCardListFragment: Fragment() {
+class FlashCardListFragment : Fragment(R.layout.fragment_flashcard_list) {
 
+    private val binding by viewBinding(FragmentFlashcardListBinding::bind)
 
-    companion object{
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fabFlashcardList.setOnClickListener {
+            findNavController().navigate(
+                R.id.presentFlashCardCreate
+            )
+        }
+    }
+
+    companion object {
         const val EXTRA_CATEGORY_ID = "EXTRA_CATEGORY_ID"
     }
 }
