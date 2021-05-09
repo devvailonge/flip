4e0b4b1772f925aa0735dedd6c12a3723b79ed5a
Roque Buarque <roque.buarque@share-now.com>
2021-05-09 08:47:42 +0200

    category list click listener

--- app/src/main/java/com/devvailonge/flip/features/categories/list/presentation/ui/CategoryListFragment.kt
+++ app/src/main/java/com/devvailonge/flip/features/categories/list/presentation/ui/CategoryListFragment.kt
@@ -1,24 +1,27 @@
 package com.devvailonge.flip.features.categories.list.presentation.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.devvailonge.flip.R
 import com.devvailonge.flip.databinding.FragmentCategoryListBinding
+import com.devvailonge.flip.features.categories.data.CategoryEntity
 import com.devvailonge.flip.features.categories.list.presentation.CategoryListEvent
 import com.devvailonge.flip.features.categories.list.presentation.CategoryListState
 import com.devvailonge.flip.features.categories.list.presentation.CategoryListViewModel
+import com.devvailonge.flip.features.flashcard.list.presentation.ui.FlashCardListFragment
 import com.devvailonge.flip.utils.viewBinding
 
 
-class CategoryListFragment: Fragment(R.layout.fragment_category_list) {
+class CategoryListFragment : Fragment(R.layout.fragment_category_list) {
 
     private val binding by viewBinding(FragmentCategoryListBinding::bind)
-    private val adapter by lazy { CategoryListAdapter() }
+    private val adapter by lazy { CategoryListAdapter(::categoryClickListener) }
+
 
     private lateinit var viewModel: CategoryListViewModel
 
@@ -42,6 +45,24 @@ class CategoryListFragment: Fragment(R.layout.fragment_category_list) {
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        setObserver()
+        viewModel.dispatch(CategoryListEvent.Fetch)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.rvCategoryList.adapter = adapter
+        binding.fabCategoryList.setOnClickListener {
+            findNavController().navigate(
+                R.id.presentCreateCategory
+            )
+        }
+    }
+
     private fun setObserver() {
         viewModel.state.observe(this, {
             updateState(it)
@@ -49,7 +70,6 @@ class CategoryListFragment: Fragment(R.layout.fragment_category_list) {
     }
 
     private fun updateState(state: CategoryListState) {
-        Log.d("Roquee", state.toString())
         when (state) {
             is CategoryListState.CategoryList -> {
                 binding.vfCategoryList.displayedChild = SUCCESS
@@ -73,24 +93,15 @@ class CategoryListFragment: Fragment(R.layout.fragment_category_list) {
         }
     }
 
-    override fun onStart() {
-        super.onStart()
-        setObserver()
-        viewModel.dispatch(CategoryListEvent.Fetch)
-
-    }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.rvCategoryList.adapter = adapter
-        binding.fabCategoryList.setOnClickListener {
-            findNavController().navigate(
-                R.id.presentCreateCategory
-            )
-        }
+    private fun categoryClickListener(categoryEntity: CategoryEntity) {
+        findNavController().navigate(
+            R.id.presentFlashCardList,
+            bundleOf(FlashCardListFragment.EXTRA_CATEGORY_ID to categoryEntity.id)
+        )
     }
 
-    companion object{
+
+    companion object {
         private const val LOADING = 0
         private const val EMPTY = 1
         private const val SUCCESS = 2
--- app/src/main/java/com/devvailonge/flip/features/flashcard/list/presentation/ui/FlashCardListFragment.kt
+++ app/src/main/java/com/devvailonge/flip/features/flashcard/list/presentation/ui/FlashCardListFragment.kt
@@ -3,4 +3,9 @@ package com.devvailonge.flip.features.flashcard.list.presentation.ui
 import androidx.fragment.app.Fragment
 
 class FlashCardListFragment: Fragment() {
+
+
+    companion object{
+        const val EXTRA_CATEGORY_ID = "EXTRA_CATEGORY_ID"
+    }
 }
