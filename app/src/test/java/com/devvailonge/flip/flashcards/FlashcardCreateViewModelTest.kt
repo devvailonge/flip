ca6a5fae1c7292e81f55955b7303453500c57a6f
Roque Buarque <roque.buarque@share-now.com>
2021-05-09 10:07:55 +0200

    fix CreateCategoryViewModelTest

---
+++ app/src/test/java/com/devvailonge/flip/flashcards/FlashcardCreateViewModelTest.kt
@@ -0,0 +1,32 @@
+package com.devvailonge.flip.flashcards
+
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class FlashcardCreateViewModelTest {
+/*
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    private val createUseCase: CreateCategoryUseCase = mock()
+    private val testee = CategoryCreateViewModel(createUseCase)
+
+    @Test
+    fun `test create use case`() {
+        //Given
+        val name = "Category Name"
+        val categoryImage = CategoryImage.HISTORY
+        val state = CategoryCreateState.Loading
+        whenever(createUseCase.perform(name, categoryImage)).thenReturn(MutableLiveData(state))
+
+        //When
+        //testee.dispatch(CategoryCreateEvent.Insert(name, categoryImage))
+        val result = testee.state.waitForValue()
+
+        //Then
+        assertEquals(state, result)
+        verify(createUseCase).perform(name, categoryImage)
+
+    }*/
+}
